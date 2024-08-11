3aa5f11dd87047ecda934c530726e936f12833de
Amar Gill <amar.s.gill@outlook.com>
2024-08-11 02:38:31 -0400

    handle pending and error states

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -3,13 +3,24 @@
 import { useTodosQuery } from "../feature/todos/queries";
 
 export default function Todos() {
-
   const { data: todos, status, error } = useTodosQuery('done')
+
+  if (status === 'pending') {
+    return (
+      <p>Loading...</p>
+    )
+  }
+
+  if (status === 'error') {
+    return (
+      <p>Error</p>
+    )
+  }
 
   return (
     <div>
       <p>TODOSSS</p>
-      {todos?.map((t) => t.title)}
+      {todos?.length ? todos.map((t) => t.title) : <p>No Todos</p>}
     </div>
   );
 }
